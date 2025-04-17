c32218a1e656c98b06104cb64746555350b6c044
keunmo-jeremy-kwak <130194888+keunmo-jeremy-kwak@users.noreply.github.com>
2025-04-18 00:39:49 +0900

    Update complete.js

--- public/complete.js
+++ public/complete.js
@@ -1,15 +1,22 @@
-(function () {
-  const params = new URLSearchParams(window.location.search);
-  const media = params.get("media");
-  const userkey = params.get("userkey");
-  const key = `ot_complete_${userkey}`;
+const params = new URLSearchParams(window.location.search);
+const media = params.get("media");
+const userkey = params.get("userkey");
 
-  if (!sessionStorage.getItem(key)) {
-    fetch("https://ot-tracker-api.onrender.com/track/complete", {
-      method: "POST",
-      body: JSON.stringify({ media, userkey, event: "complete" }),
-      headers: { "Content-Type": "application/json" },
-    });
-    sessionStorage.setItem(key, "1");
-  }
-})();
+console.log("📦 media:", media);
+console.log("📦 userkey:", userkey);
+
+const key = `ot_complete_${userkey}`;
+
+if (!sessionStorage.getItem(key)) {
+  fetch("https://ot-tracker-api.onrender.com/track/complete", {
+    method: "POST",
+    body: JSON.stringify({ media, userkey, event: "complete" }),
+    headers: { "Content-Type": "application/json" },
+  }).then(() => {
+    console.log("✅ 전송 완료!");
+  }).catch((err) => {
+    console.error("❌ 전송 실패", err);
+  });
+  
+  sessionStorage.setItem(key, "1");
+}
